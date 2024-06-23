f9a94d2df4843393a76a1cb34a972cc3b8ba2b6f
Tanvi Laddha <67188868+tnv-9217@users.noreply.github.com>
2024-06-23 14:12:18 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello! this is TnV Laddha and I am a business applications analyst at AUC LLP.';
+    return 'Hello! this is TnV Laddha and I am a business applications analyst at AUC LLP. This is task submission for PearlThoughts Devops internship';
   }
 }
